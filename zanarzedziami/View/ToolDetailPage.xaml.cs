0a088a8509125038e63365ad6de0baba1d1edc4a
teren <teren@KOMP>
2025-04-07 17:13:35 +0200

    dokumentacja

--- zanarzedziami/View/ToolDetailPage.xaml.cs
+++ zanarzedziami/View/ToolDetailPage.xaml.cs
@@ -3,15 +3,21 @@ using zanarzedziami.Model;
 using Microsoft.Maui.Controls.Xaml;
 using zanarzedziami.ViewModel;
 using zanarzedziami.Service;
-using zanarzedziami.ViewModel;
 
 namespace zanarzedziami.View
 {
+
     public partial class ToolDetailPage : ContentPage
     {
         private Tool tool;
         private ToolViewModel viewModel;
 
+
+        /// <summary>
+        /// Konstruktor strony szczegó³ów narzêdzia.Widok modelu oraz ustawia kontekst danych na wybrane narzêdzie.
+        /// </summary>
+        /// <param name="selectedTool">Obiekt narzêdzia, którego szczegó³y bêd¹ wyœwietlane.</param>
+        /// <param name="vm">Widok modelu zawieraj¹cy logikê aplikacji dla narzêdzi</param>
         public ToolDetailPage(Tool selectedTool, ToolViewModel vm)
         {
             InitializeComponent();
@@ -20,16 +26,27 @@ namespace zanarzedziami.View
             BindingContext = tool;
         }
 
+        /// <summary>
+        /// Obs³uguje klikniêcie przycisku usuwania narzêdzia. Potwierdza usuniêcie narzêdzia i wykonuje.
+        /// </summary>
+        /// <param name="sender">Obiekt, który wywo³a³ zdarzenie </param>
+        /// <param name="e">Dodatkowe informacje.</param>
         private async void DeleteTool(object sender, EventArgs e)
         {
             bool confirm = await DisplayAlert("Potwierdzenie", "Na pewno usun¹æ?", "Tak", "Nie");
             if (confirm)
             {
-                await viewModel.DeleteToolAsync(tool);
-                await Navigation.PopAsync();
+                await viewModel.DeleteToolAsync(tool);  //Jeœli u¿ytkownik potwierdzi, usuwamy narzêdzie
+                await Navigation.PopAsync(); //Powrót do poprzedniej strony
             }
         }
 
+
+        /// <summary>
+        /// Obs³uguje klikniêcie przycisku aktualizacji narzêdzia. Potwierdza aktualizacjê narzêdzia i wykonuje.
+        /// </summary>
+        /// <param name="sender">Obiekt, który wywo³a³ zdarzenie</param>
+        /// <param name="e">dodatkowe informacje</param>
         private async void UpdateTool(object sender, EventArgs e)
         {
             bool confirm = await DisplayAlert("Potwierdzenie", "Zaktualizowaæ narzêdzie?", "Tak", "Nie");
